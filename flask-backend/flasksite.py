c3ebd7e32464ce574e0450444a19fe30aa46c9e3
Colin Luu <colinluu@Colins-MacBook-Air.local>
2019-09-06 01:06:28 +1000

    Login Page

--- flask-backend/flasksite.py
+++ flask-backend/flasksite.py
@@ -1,6 +1,6 @@
-import flask 
+from flask import *
 import pyrebase
-app = flask.Flask("__main__")
+app = Flask("__main__")
 
 config = {
 	"apiKey": "AIzaSyDDeGbUcZmqJDDn7KkO8sQlwMsDYSAU7DY",
@@ -13,13 +13,39 @@ config = {
 }
 
 firebase = pyrebase.initialize_app(config)
-db = firebase.database()
 
-db.child("names").push({"name": "Jack"})
+#db = firebase.database()
+
+#db.child("names").push({"name": "Jack"})
+
+#Register Login
+
+
+auth = firebase.auth()
+
+#email = input ('Please enter your email\n')
+#password = input('Please enter your password\n')
+
+#user = auth.create_user_with_email_and_password(email, password)
+
+#auth.get_account_info(user['idToken'])
 
 @app.route("/")
 def my_index():
-	return flask.render_template("index.html", token = "Hello Flask + React!")
+	return render_template("index.html", token = "Hello Flask + React!")
+
+#At the url /login, Login page
+
+@app.route("/login", methods=['GET', 'POST' ])
+
+def login():
+	if request.method == 'POST':
+		email = request.form['name']
+		password = request.form['pass']
+		auth.sign_in_with_email_and_password(email, password)
+		return 'Login Successful'
+	return render_template('login.html')
 
 #Allows us to save changes and have them reflect immediately on the webpage without restarting the web server
-app.run(debug = True)
+app.run(debug = True)	
+
